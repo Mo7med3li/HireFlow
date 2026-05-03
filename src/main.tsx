07946998ef4e2c4aaa50bf4b14c4b81b9f500b18
Mohamed Ali <30309101700696@ci.menofia.edu.eg>
2026-05-04 02:43:42 +0300

    refactor name and structure

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./app/pages/home/App";
+import App from "./app/pages/home/home-page";
 import Providers from "./components/providers";
 import PagesLayout from "./app/pages/pagesLayout";
 const router = createBrowserRouter([
